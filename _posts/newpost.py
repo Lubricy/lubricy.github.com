04dbe1178b646376be007f48b6adb9dcd619f805
lubricy <lubricy@gmail.com>
2013-07-06 15:15:37 +0800

    blog update

---
+++ _posts/newpost.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-*- encoding:utf-8 -*-
+import os
+import time
+
+now = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+title = raw_input("title:");
+categories = raw_input("categories");
+filename = "%s-%s.md" % (now,title)
+f = open(filename,'w')
+now = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+text = """---
+layout: post
+title:  "%s"
+date:   %s
+categories: %s
+---
+
+""" % (title,now,categories)
+f.write(text)
+f.close()
+os.system("vim " + filename)
