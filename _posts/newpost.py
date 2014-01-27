6c9c508e5c25b5cd61df792d7d3316138e11b8e7
Lubricy <lubricy@gmail.com>
2014-01-27 16:38:20 +0800

    .

--- _posts/newpost.py
+++ _posts/newpost.py
@@ -4,8 +4,8 @@ import os
 import time
 
 now = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-title = raw_input("title:");
-categories = raw_input("categories");
+title = raw_input("title:\t");
+categories = raw_input("categories:\t");
 filename = "%s-%s.md" % (now,title)
 f = open(filename,'w')
 now = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
